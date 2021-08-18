2a5f3295ae3c4c0f16a0d9ccd8e3ac1acc11151e
Alexey Salazkin <salazkin@gmail.com>
2021-08-18 19:01:11 +0300

    cleaning

--- components/NineSliceImage.ts
+++
@@ -1,8 +0,0 @@
-import {NineSlicePlane} from "pixi.js";
-import Resources from "../services/Resources";
-
-export default class NineSliceImage extends NineSlicePlane {
-    constructor(textureId: string, left: number, top: number, right: number, bottom: number) {
-        super(Resources.getTexture(textureId), left, top, right, bottom);
-    }
-}
